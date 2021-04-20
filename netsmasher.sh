7f5580d9e19ebb0481a8ddfa64b5ddd094e157ae
Passhyk <70662829+Passhyk@users.noreply.github.com>
2021-04-20 16:09:36 +0300

    scan version added

--- netsmasher.sh
+++ netsmasher.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-
+clear
 banner (){
+
 echo " _ __   __ _ ___ ___| |__  _   _| | _| |"
 echo "| '_ \ / _  / __/ __  | '_ \| | | |  |/|"
 echo "| |_) | (_| \__ \__ \ | | | |_| |   <|_|"
@@ -27,5 +28,39 @@ banner
 printf "\n ips listed below are in your network!\n\n"
 ip
 
-printf "\n and this is your ip address!\n\n" 
+printf "\n your ip is:\n\n" 
 myip 
+
+echo -e "\n\n[hit enter to continue] "
+read -p ' ' ohayogosayumasu
+
+scan(){
+echo -e "\n ~do you want to also scan these fellows?~"
+read -p $'THEN  yes/no >_: ' bakayaro
+if [[ "$bakayaro" == "yes" ]]; then
+        clear
+        
+        ip > scan.txt 
+        echo -e "\n SCANNING ..."
+        
+        cat scan.txt
+        echo -e "SCANNING ..."
+        echo -e "\n\n[created a text file of ip addresses]\n "
+        read -p '[hit enter to continue]' konichiwagosayumasu
+        for ip in $(cat scan.txt);
+        do nmap -p 80 -T4 $ip &
+       
+        
+        read -p ' '
+        done
+    elif [[ "$bakayaro" == "no" ]]; then
+        echo -e "LEAVING ..."
+
+        exit 0
+    else
+            echo -e "\n INVALID VALUE!"
+   	    scan
+fi
+}
+
+scan
